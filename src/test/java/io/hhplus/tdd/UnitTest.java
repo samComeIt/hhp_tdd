9774a921b0c2bc876cdbd9cd98795c0370617073
김산해 <sanhae1996@gmail.com>
2024-06-19 23:52:19 +0900

    UnitTest 수정

--- src/test/java/io/hhplus/tdd/UnitTest.java
+++ src/test/java/io/hhplus/tdd/UnitTest.java
@@ -19,69 +19,106 @@ public class UnitTest {
     private UserPoint userPoint;
 
     @Test
-    @DisplayName("ID 0인 경우")
-    void testId()
+    @DisplayName("ID 0인 경우 실패")
+    void testIdIs0()
     {
-        Long userId = 0L;
-        assertEquals(0, userId);
+        // given
+        Long userId01 = 0L;
+
+        // when, then
+        assertTrue(userId01 == 0);
     }
 
     @Test
-    @DisplayName("0포인트 사용/추가 경우")
+    @DisplayName("ID null인 경우 실패")
+    void testIdIsNUll()
+    {
+        // given
+        Long userId01 = null;
+
+        // when, then
+        assertTrue(userId01 == null);
+    }
+
+    @Test
+    @DisplayName("null/0 포인트 사용/추가 경우 실패")
     void testPoint()
     {
+        // given
+        Long amount01 = null;
+
+        // when
         boolean result = false;
+        if (amount01 == null || amount01 == 0) result = true;
 
-        Long amount01 = 0L;
-        if (amount01 == 0) result = true;
-
+        // then
         assertTrue(result);
-        assertFalse(result);
     }
 
     @Test
-    @DisplayName("현재 포인트 조회")
+    @DisplayName("현재 포인트 조회 성공")
     void testCurPoint()
     {
-        Long id = 1L;
-        UserPoint userPoint = new UserPoint(id, 500L, System.currentTimeMillis());
+        // given
+        Long userId01 = 1L;
 
-        Long amount = 500L;
-        //userPoint.add(id, amount);
+        // when
+        UserPoint userPoint = new UserPoint(userId01, 500L, System.currentTimeMillis());
 
+        // then
         assertEquals(500L, userPoint.point());
     }
 
     @Test
-    @DisplayName("포인트 추가")
+    @DisplayName("포인트 추가 성공")
     void testAddPoint()
     {
-        Long id = 2L;
-        UserPoint userPoint = new UserPoint(id, 300L, System.currentTimeMillis());
+        // given
+        Long userId01 = 2L;
 
-        //userPoint.add(id, 500L);
+        // when
+        UserPoint userPoint = new UserPoint(userId01, 300L, System.currentTimeMillis());
+
+        // then
         assertEquals(300L, userPoint.point());
         assertTrue(300L == userPoint.point());
     }
 
     @Test
-    @DisplayName("포인트 사용")
+    @DisplayName("포인트 사용 성공")
     void testUsePoint()
     {
-        Long id = 2L;
-        UserPoint userPoint = new UserPoint(id, 300L, System.currentTimeMillis());
+        // given
+        Long userId01 = 2L;
+        Long point = 300L;
+        UserPoint userPoint = new UserPoint(userId01, point, System.currentTimeMillis());
+
+        Long use = 100L;
+
+        // when
+        Long amount = point - use;
+
+        userPoint = new UserPoint(userId01, amount, System.currentTimeMillis());
+
+        // then
+        assertEquals(200L, userPoint.point());
+        assertEquals(amount, userPoint.point());
+        assertEquals(amount, 200L);
     }
 
     @Test
-    @DisplayName("포인트 추가 내역 조회")
+    @DisplayName("포인트 추가 내역 조회 성공")
     void testGetAddPointHistoryList()
     {
+        // given
         List<PointHistory> table = new ArrayList<>();
         table.add(new PointHistory(1L, 100L, 200L, TransactionType.CHARGE, System.currentTimeMillis()));
         table.add(new PointHistory(1L, 100L, 200L, TransactionType.USE, System.currentTimeMillis()));
 
+        // when
         int cnt = table.stream().filter(pointHistory -> pointHistory.type() == TransactionType.CHARGE).toList().size();
 
+        // then
         assertEquals(1, cnt);
         assertFalse(2 == cnt);
     }
