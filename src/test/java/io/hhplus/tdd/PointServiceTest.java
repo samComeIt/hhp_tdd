34dcbaf228147635a390807a66043396b526d146
김산해 <sanhae1996@gmail.com>
2024-06-20 00:09:16 +0900

    Service Test 개선

--- src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -19,37 +19,46 @@ public class PointServiceTest {
     private PointService pointService;
 
     /**
+     * 포인트 사용전에 현재 잔고가 0보다 작거나 같으면 실패
      * 잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다
      * 동시에 여러 건의 포인트 충전, 이용 요청이 들어올 경우 순차적으로 처리되어야 합니다.
      */
 
     @Test
-    @DisplayName("잔고가 부족하는지 확인")
+    @DisplayName("포인트 사용전에 현재 잔고가 0보다 작거나 같으면 실패")
     void testCurPoint()
     {
-        Long id = 100L;
-        UserPoint userPoint = pointService.getPoint(id);
+        // given
+        Long userId01 = 100L;
 
+        // when
+        UserPoint userPoint = pointService.getPoint(userId01);
+
+        //then
         assertEquals(userPoint.point(), 0);
         assertFalse( userPoint.point() > 0);
     }
 
     @Test
-    @DisplayName("히스토리 추가")
+    @DisplayName("히스토리 추가 성공")
     void testAddHistory()
     {
-        Long id = 100L;
+        // given
+        Long userId01 = 100L;
         Long point = 200L;
 
+        // when
         PointHistoryDto pointHistoryDto = new PointHistoryDto();
-        pointHistoryDto.setUserId(id);
+        pointHistoryDto.setUserId(userId01);
         pointHistoryDto.setAmount(point);
         pointHistoryDto.setType(TransactionType.CHARGE);
         pointHistoryDto.setUpdateMillis(System.currentTimeMillis());
 
         pointService.addHistory(pointHistoryDto);
 
-        int cnt = pointService.findAllHistoryById(id).size();
+        int cnt = pointService.findAllHistoryById(userId01).size();
+
+        // then
         assertEquals(1, cnt);
     }
 
